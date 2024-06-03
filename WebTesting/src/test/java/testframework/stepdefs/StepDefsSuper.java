7b55e8187388e45183b6454f6de906cf01d17864
RazorWinds <logan.marshall.dev@gmail.com>
2024-06-03 13:50:37 +0100

    Reverted last update

--- WebTesting/src/test/java/testframework/stepdefs/StepDefsSuper.java
+++ WebTesting/src/test/java/testframework/stepdefs/StepDefsSuper.java
@@ -26,23 +26,27 @@ public abstract class StepDefsSuper {
         return chromeOptions;
     }
 
-    public static void beforeAll() throws IOException {
-        service = new ChromeDriverService.Builder()
-                .usingDriverExecutable(new File(DRIVER_LOCATION))
-                .usingAnyFreePort()
-                .build();
-        service.start();
-    }
-
-    public void setup() {
-        webDriver = new RemoteWebDriver(service.getUrl(), getChromeOptions());
-    }
-
-    public void afterEach() {
-        webDriver.quit();
-    }
-    
-    public static void afterAll() {
-        service.stop();
-    }
+//    @BeforeAll
+//    public static void beforeAll() throws IOException {
+//        service = new ChromeDriverService.Builder()
+//                .usingDriverExecutable(new File(DRIVER_LOCATION))
+//                .usingAnyFreePort()
+//                .build();
+//        service.start();
+//    }
+//
+//    @Before
+//    public void setup() {
+//        webDriver = new RemoteWebDriver(service.getUrl(), getChromeOptions());
+//    }
+//
+//    @After
+//    public void afterEach() {
+//        webDriver.quit();
+//    }
+//
+//    @AfterAll
+//    public static void afterAll() {
+//        service.stop();
+//    }
 }
